175d63c836537cf0fab97a10567acc43010862b2
Maanas-bit <20MJakhar@ceciljones.net>
2024-10-08 12:32:38 +0000

    fixed /

--- app/media/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Media(){
-    return(
-        <div>
-            <h1>
-                hello from media
-            </h1>
-        </div>
-    )
-}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -14,14 +14,14 @@ export default function NotFound() {
     router.back();
   };
 
-  const pathname = usePathname();
+  const pathname: string = usePathname().replace(/\//g, "");
 
   const notify = () => toast("Cool!");
 
   return (
     <div className='overflow-hidden h-screen'>
         <h1 className='text-center mt-20 text-[50px] text'>Looks like someone messed up</h1>
-        <h1 className='text-center text-[40px] text-indigo-600'>The URL "{pathname}" does not exist.</h1>
+        <h1 className='text-center text-[40px] text-indigo-400'>The URL "{pathname}" does not exist.</h1>
         <h1 className='text-violet-700 text-[400px] text-center' >404</h1>
           <div className='w-screen text-center h-screen'>
             <Button onClick={goBack} variant="extra" className='text-center p-8 m-2 text-xl font-bold' >Go back</Button>
